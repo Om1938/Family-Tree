7d241b8d7effd85eb8c111d732651599142492d1
stek <pvhittalamani@gmail.com>
2018-07-26 15:17:21 +0530

    26/7/18 15:17

--- src/app/data-service.service.ts
+++ src/app/data-service.service.ts
@@ -8,10 +8,40 @@ import { map } from 'rxjs/operators';
 export class DataServiceService {
 
   constructor(private http:HttpClient) { }
-  api:string ="http://192.168.32.229:3000";
+  api:string ="http://192.168.42.156:3000";
 
   getData(){
     console.log("Call to hua hai"); 
+    const options ={
+      headers:new HttpHeaders({
+        'Content-Type':'application/json'
+      })
+    };
+    return this.http.get(this.api + '/getdata');
+  }
+
+  addNode(data){
+    console.log(data); 
+    const options ={
+      headers:new HttpHeaders({
+        'Content-Type':'application/json'
+      })
+    };
+    return this.http.post(this.api + '/register',{},options);
+  }
+
+  addRelation(data){
+    console.log(data); 
+    const options ={
+      headers:new HttpHeaders({
+        'Content-Type':'application/json'
+      })
+    };
+    return this.http.get(this.api + '/getdata');
+  }
+
+  delNode(data){
+    console.log(data); 
     const options ={
       headers:new HttpHeaders({
         'Content-Type':'application/json'
--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -7,6 +7,14 @@ import { DataServiceService } from '../data-service.service';
   styleUrls: ['./sidenav.component.css']
 })
 export class SidenavComponent implements OnInit {
+  username: string = null;
+  password: string = null;
+
+  user1:string = null;
+  relation: string = null;
+  user2: string = null;
+
+  user: string = null;
 
   constructor(private dataservice:DataServiceService) { }
 
@@ -14,6 +22,17 @@ export class SidenavComponent implements OnInit {
   }
   adduser(){
     console.log("gg");
+    var data = this.dataservice.addNode({user:this.username,password:this.password});
+    console.log(data);
+  }
+  addrelation(){
+    console.log("gg");
+    var data = this.dataservice.addRelation({user1:this.user1,realtion:this.relation,user2:this.user2});
+    console.log(data);
+  }
+  delnode(){
+    console.log("gg");
+    this.dataservice.delNode({user:this.user});
   }
 
 }
